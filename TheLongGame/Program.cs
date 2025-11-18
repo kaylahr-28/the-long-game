67a133e18f70c1436d64fef980a66ae0a9f3559a
kaylahr-28 <kaylahrouzier28@gmail.com>
2025-11-18 11:58:47 -0500

    all done - need to fix: -adds point to score after entering name -need to press enter 2x to exit

--- TheLongGame/Program.cs
+++ TheLongGame/Program.cs
@@ -1,17 +1,25 @@
 ﻿using System.Diagnostics.Metrics;
 using System.Drawing;
+using System.IO;
 using System.Xml.Linq;
 
 namespace TheLongGame
 {
 
+    //class that holds current user's name and score
     public class User
     {
         public string name;
         public int score; 
-        public User(string name, int score) {
-            this.name = "N/A";
+        // By default, the player starts with a score of 0.
+        public User(string name) {
+            this.name = name;
             this.score = 0;
+        }
+
+        public void UpdateScore(StreamWriter writer)
+        {
+            writer.Write(this.score);
         }
     }
     internal class Program
@@ -20,38 +28,51 @@ namespace TheLongGame
         {
 
             //When the program starts, ask the user to enter their name.
-                Console.WriteLine("Hi! Please enter your name:");
-                string newName = Console.ReadLine();
+            Console.WriteLine("Hi! Please enter your name:");
+            string newName = Console.ReadLine();
 
-            string currentDirectory = Directory.GetCurrentDirectory();
-            Console.WriteLine(currentDirectory);
+            User user = new User(newName);
+
+            string path = "../../../" + newName + ".txt";
+
+            //  When a user enters a name at the start, if they already have a previously saved score, start with that score instead.
             //if name alr exists, retrieve .txt file
-
-            //change so newName is var not string
-            if (File.Exists(currentDirectory + newName + ".txt"){
-                FileStream f = new FileStream(newName+".txt", FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
-
-                User user = new User(newName, int.Parse(File.OpenRead(currentDirectory + newName + ".txt"));
-            }
-            else
-            {
-                using (StreamWriter writer = File.CreateText("newName.txt"))
-                    User user = new User(newName, 0);
-
+            if (File.Exists(path)){
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    //retrieve existing score
+                    user.score = int.Parse(reader.ReadLine());
                 }
+            }
+            else //create new file for user
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    user.UpdateScore(writer);
+                }
+                   
+                };
 
+              Console.WriteLine("Hello, " + newName + "! Your current score is " + user.score + ". Press a key to add to your score, or press enter to exit.");
+             // Add 1 point to their score for every keypress they make.
+                do {
+                    user.score++;
+             // Display the player’s updated score after each keypress.
+                Console.WriteLine("Your score is now " + user.score + ". Press a key to add to your score, or press enter to exit.");
+                } while (Console.ReadKey(true).Key != ConsoleKey.Enter);
 
-                   Console.WriteLine("Hello, " + newName + "! Your current score is " + user.score + ". Press a key to add to your score, or press enter to exit.");
-
-
-                // By default, the player starts with a score of 0.
-                // Add 1 point to their score for every keypress they make.
-                // Display the player’s updated score after each keypress.
-                // When the player presses the Enter key, end the game.
-                // Hint: the following code reads a keypress and checks whether it was the Enter key: Console.ReadKey().Key == ConsoleKey.Enter
-                //  When the player presses Enter, save their score in a file.
-                // Hint: Consider saving this to a file named[username].txt.For this challenge, you can assume the user doesn’t enter a name that would produce an illegal file name(such as *).
-                //  When a user enters a name at the start, if they already have a previously saved score, start with that score instead.
+             // When the player presses the Enter key, end the game.
+            if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine("Your final score is " + user.score + ". Thanks for playing!");
+              // When the player presses Enter, save their score in a file.
+              //recreate text file and add updated score
+               using (StreamWriter writer = new StreamWriter(path,false))
+                {
+                    user.UpdateScore(writer);
+                }
+            }
+  
             }
         }
 }
